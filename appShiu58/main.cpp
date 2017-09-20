2844c8d1f6e2cbb47f3baf9a7e284e57b486ac0b
ZHONG SIFEN <zhongsifen@gmail.com>
2017-09-20 17:26:06 +0800

    main

--- appShiu58/main.cpp
+++ appShiu58/main.cpp
@@ -7,45 +7,63 @@
 //
 
 #include "Shiu58.hpp"
+#include "ShiuBgs.hpp"
+#include "ShiuProc.hpp"
 
 #include <iostream>
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
 #include <opencv2/videoio.hpp>
 using namespace cv;
+
+const std::string _data("/Users/zhongsifen/Work/Shiu58/data/");
+//const std::string _name("20170915140730.mp4");
+//const std::string _name("20170915140822.mp4");
+const std::string _name("20170915140930.mp4");
+const std::string _path(_data + _name);
+
+#define WITH_FILE
+//#define WITH_REC
 
 int main(int argc, const char * argv[]) {
 	bool ret = false;
 	char key = '\0';
 
-	Shiu58 shiu;
-	ret = shiu.load();	if (!ret) return -1;
-	
-	Mat f, g, h, w;
-	std::vector<Mat> hsl;
-	
-//	Rect box;
-//	int level=0;
-//	double weight=0;
-	const int waittime = 1;
+	Mat f, w, mask;
 	VideoCapture cap;
-	cap.open(0);		if (!cap.isOpened()) return -1;
+#if defined(WITH_FILE)
+	cap.open(_path);
+#else
+	cap.open(0);
+#endif
+	if (!cap.isOpened()) return -1;
 	ret = cap.read(f);		if (!ret) return -1;
-
-	do {
-		ret = cap.read(f);		if (!ret) continue;
+#if defined(WITH_REC)
+	VideoWriter rec;
+	rec.open(_data + "rec264.avi", VideoWriter::fourcc('X','2','6','4'), 25.0, Size(f.cols, f.rows));	if (!rec.isOpened()) return  -1;
+#endif
+	
+	while (true) {
+		ret = cap.read(f);		if (!ret) break;
+		ShiuProc::process(f, mask);
+		
 		w = f.clone();
+		w = Scalar(0xFF, 0xFF, 0xFF);
+		f.copyTo(w, mask);
 		
-		ret = shiu.run(f);
-		
-		if (ret) {
-			shiu.show(w);
-		}
-		imshow("Shiu58", w);
-		key = waitKey(waittime);		if (key == 'q') break;
-	} while (1);
+#if defined(WITH_REC)
+		rec.write(w);
+#endif
+		key = waitKey(1);		if (key == 27) break;
+	}
 
+#if defined(WITH_REC)
+	rec.release();
+#endif
+	cap.release();
+	
 	std::cout << "Hello, World!\n";
 	return 0;
 }
